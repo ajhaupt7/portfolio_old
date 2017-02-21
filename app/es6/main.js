79d883d9337d6cfbc8f728d7312aa608a292e223
Andrew Haupt <ajhaupt7@gmail.com>
2017-02-20 16:04:22 -0800

    Displays projects and can switch between them

--- app/es6/main.js
+++ app/es6/main.js
@@ -1,1 +1,46 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    projects: projects,
+    activeProject: null,
+    hoverTitle: null,
+    hoverStyles: {}
+  },
+  methods: {
+    getBackgroundStyles(project) {
+      return { backgroundImage: `url(images/logos/${project.logo})` };
+    },
+    toggleHover(event, project, hovering) {
+      if (hovering) {
+        project.hoverStyles = { backgroundColor: project.primaryColor, boxShadow: project.boxShadow };
+        this.hoverStyles = { backgroundColor: project.primaryColor, boxShadow: project.boxShadow };
+        this.hoverTitle = project.name;
+      } else {
+        project.hoverStyles = { backgroundColor: 'white' };
+        this.hoverTitle = null;
+      }
+    },
+    assignPrimaryColors() {
+      this.projects.map((project) => {
+        const idx = Math.floor(Math.random() * primaryColors.length);
+        const attrs = primaryColors[idx];
+        project.primaryColor = `rgba(${attrs[0]},${attrs[1]},${attrs[2]}, 1)`;
+        project.boxShadow = `rgba(${attrs[0]},${attrs[1]},${attrs[2]}, 0.7)`;
+      });
+    },
+    setActiveProject(event, project) {
+      const tile = event.target.closest(".tile");
+      this.activeProject = project || null;
+      tile.classList.add('pulse');
+      setTimeout(() => {
+        tile.classList.remove('pulse');
+      }, 1200);
+    },
+    makeSubtitle(arr) {
+      return arr.join(" + ");
+    }
+  }
+});
 
+// DOM Ready
+app.assignPrimaryColors();
--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,48 @@
-"use strict";
+'use strict';
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    projects: projects,
+    activeProject: null,
+    hoverTitle: null,
+    hoverStyles: {}
+  },
+  methods: {
+    getBackgroundStyles: function getBackgroundStyles(project) {
+      return { backgroundImage: 'url(images/logos/' + project.logo + ')' };
+    },
+    toggleHover: function toggleHover(event, project, hovering) {
+      if (hovering) {
+        project.hoverStyles = { backgroundColor: project.primaryColor, boxShadow: project.boxShadow };
+        this.hoverStyles = { backgroundColor: project.primaryColor, boxShadow: project.boxShadow };
+        this.hoverTitle = project.name;
+      } else {
+        project.hoverStyles = { backgroundColor: 'white' };
+        this.hoverTitle = null;
+      }
+    },
+    assignPrimaryColors: function assignPrimaryColors() {
+      this.projects.map(function (project) {
+        var idx = Math.floor(Math.random() * primaryColors.length);
+        var attrs = primaryColors[idx];
+        project.primaryColor = 'rgba(' + attrs[0] + ',' + attrs[1] + ',' + attrs[2] + ', 1)';
+        project.boxShadow = 'rgba(' + attrs[0] + ',' + attrs[1] + ',' + attrs[2] + ', 0.7)';
+      });
+    },
+    setActiveProject: function setActiveProject(event, project) {
+      var tile = event.target.closest(".tile");
+      this.activeProject = project || null;
+      tile.classList.add('pulse');
+      setTimeout(function () {
+        tile.classList.remove('pulse');
+      }, 1200);
+    },
+    makeSubtitle: function makeSubtitle(arr) {
+      return arr.join(" + ");
+    }
+  }
+});
+
+// DOM Ready
+app.assignPrimaryColors();
